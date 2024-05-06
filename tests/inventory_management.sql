118684de53d5b15acc687cd1a8ec0ed60fbcfe9c
sm.mostafa <sm.mostafa@softrobotics.com.bd>
2024-05-06 20:22:41 +0600

    'updated'

--- tests/inventory_management.sql
+++ tests/inventory_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 04, 2024 at 11:41 PM
+-- Generation Time: May 06, 2024 at 02:21 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -33,6 +33,7 @@ CREATE TABLE `products` (
   `price` double DEFAULT NULL,
   `category` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `quantity` double DEFAULT NULL,
+  `total` double DEFAULT NULL,
   `sr_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `sr_company` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `sr_phone` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -45,8 +46,9 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `product_name`, `price`, `category`, `quantity`, `sr_name`, `sr_company`, `sr_phone`, `image`, `created_at`, `updated_at`) VALUES
-(8, 'test', 10, 'cleaners', 1, 'test', 'test', '01855536222', NULL, '2024-05-04 23:11:47', '2024-05-04 23:18:00');
+INSERT INTO `products` (`id`, `product_name`, `price`, `category`, `quantity`, `total`, `sr_name`, `sr_company`, `sr_phone`, `image`, `created_at`, `updated_at`) VALUES
+(11, 'softrobo', 12, 'others', 1, 12, 'testingtesting', 'softrobot', '01855536222', '1714919818.jpg', '2024-05-05 14:36:58', '2024-05-05 14:43:32'),
+(12, 'name', 12, 'personal_care', 12, 144, 'treset', 'coman', '01855536223', '1714920051.jpg', '2024-05-05 14:40:51', '2024-05-05 14:40:51');
 
 -- --------------------------------------------------------
 
@@ -61,6 +63,7 @@ CREATE TABLE `users` (
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -69,9 +72,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `email`, `phone`, `password`, `updated_at`, `created_at`) VALUES
-(1, 'Mostafa', 'admin', 'test@test.com', '01855536222', '$2y$10$yXfVnCnsk2IcPEaCGTt8CeOsX6dHoqWjXxy/CghIu1KA7zXeSgskG', '2024-04-28 13:17:46', '2024-04-17 14:16:30'),
-(2, 'mostafa', 'mostafa', 'test@email.com', '01855536222', '$2y$10$HoeXB24TrShJIUtf.btfHuqkaEqob9M1NBSRWEUv2XPV7woZJ7Ide', '2024-04-17 17:29:45', '2024-04-17 17:22:57');
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `phone`, `password`, `image`, `updated_at`, `created_at`) VALUES
+(1, 'Mostafa', 'admin', 'test@test.com', '01855536222', '$2y$10$yXfVnCnsk2IcPEaCGTt8CeOsX6dHoqWjXxy/CghIu1KA7zXeSgskG', '1714923339.png', '2024-05-05 15:35:39', '2024-04-17 14:16:30'),
+(2, 'mostafa', 'mostafa', 'test@email.com', '01855536222', '$2y$10$HoeXB24TrShJIUtf.btfHuqkaEqob9M1NBSRWEUv2XPV7woZJ7Ide', NULL, '2024-04-17 17:29:45', '2024-04-17 17:22:57'),
+(3, 'test', 'testing', 'test@test.com', '01855536222', '$2y$10$MuobH4kYUdeMY./87kjGRujpqFWnbKJzb1MsLz/i8QOsYEYlMG0OS', NULL, '2024-05-05 15:37:24', '2024-05-05 15:37:24'),
+(4, 'test', 'testingt', 'test@test.com', '01855536222', '$2y$10$Hlaj1FyGskX0ZE4.y.fD0OpQLCVkjh5kWNnJYj8P6pgIsFdwqAz7q', '1714923779.png', '2024-05-05 15:42:59', '2024-05-05 15:42:59');
 
 --
 -- Indexes for dumped tables
@@ -97,13 +102,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
